70988e91f6c715dceb01127922e703537ded295e
Gunko Vika <vikahunko@icloud.com>
2024-11-24 17:12:29 +0300

    Update Lab3.1.java

--- Lab_3/Lab3.1.java
+++ Lab_3/Lab3.1.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Lab3.1 {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
